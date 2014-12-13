8cb523deaf1dc066c1cc50eadc9db7aa49dad74d
Jon Mash <jonmash@gmail.com>
2014-12-12 22:40:55 -0500

    Adding fieldset and dropdowns

--- js/myForms.js
+++ js/myForms.js
@@ -14,54 +14,109 @@ jQuery(document).ready(function(){
 	};
 	
 	var default_schema = [
-					{
-						name: "user",
-						label: "User",
-						type: "text",
-						id: "username",
-						class: "",
-						value: "",
-						required: "Y",
-						placeholder: "Your Username"
-					},
-					{
-						name: "key",
-						label: "Key",
-						type: "hidden",
-						id: "key",
-						class: "key",
-						value: "1234567",
-						required: "N"
-					},
-					{
-						name: "email",
-						label: "Email",						
-						type: "email",
-						id: "email",
-						class: "",
-						value: "",
-						required: "Y",
-						placeholder: "Your Email Address"
-					},
-					{
-						name: "password",
-						label: "Password",						
-						type: "password",
-						id: "pass",
-						class: "passField",
-						value: "",
-						placeholder: "Your Password"
-					},
-					{
-						name: "submit",
-						type: "submit",
-						id: "submitBtn",
-						class: "button",
-						value: "Save"
-					}
-				];
-	$('#genSchema').val(JSON.stringify(default_schema, null, "\t"));
-	$('#genOptions').val(JSON.stringify(default_options, null, "\t"));
+		{
+			"name": "key",
+			"label": "Key",
+			"type": "hidden",
+			"id": "key",
+			"class": "key",
+			"value": "1234567",
+			"required": "N"
+		},
+		{
+			"name":"set1",
+			"label": "Login",
+			"type": "fieldset",
+			"inherit" : "true",
+			"fields":[
+				{
+					"name": "user",
+					"label": "User",
+					"type": "text",
+					"id": "username",
+					"class": "",
+					"value": "",
+					"required": "Y",
+					"placeholder": "Your Username"
+				},
+				{
+					"name": "password",
+					"label": "Password",
+					"type": "password",
+					"id": "pass",
+					"class": "passField",
+					"value": "",
+					"placeholder": "Your Password"
+				}
+			]
+		},
+		{
+			"name": "email",
+			"label": "Email",
+			"type": "email",
+			"id": "email",
+			"class": "",
+			"value": "",
+			"required": "Y",
+			"placeholder": "Your Email Address"
+		},
+		{
+			"name": "dropdown1",
+			"label": "Dropdown",
+			"type": "dropdown",
+			"id": "dd1",
+			"class": "",
+			"selected":"opt2",
+			"options": [
+				{
+					"name":"opt1",
+					"value":"Option #1"
+				},{
+					"name":"opt2",
+					"value":"Option #2"
+				},{
+					"name":"opt3",
+					"value":"Option #3",
+				}
+			]
+		},
+		{
+			"name": "dropdown2",
+			"label": "Dropdown2",
+			"type": "dropdown",
+			"id": "dd2",
+			"class": "",
+			"options": [
+				{
+					"name":"opt1",
+					"value":"Option #1"
+				},{
+					"name":"opt2",
+					"value":"Option #2"
+				},{
+					"name":"opt3",
+					"value":"Option #3",
+					"selected":"true"
+				}
+			]
+		},
+		{
+			"name": "submit",
+			"type": "submit",
+			"id": "submitBtn",
+			"class": "button",
+			"value": "Save"
+		},
+		{
+			"name": "reset",
+			"type": "reset",
+			"id": "resetBtn",
+			"class": "button",
+			"value": "Reset"
+		}
+	];
+	$('#genSchema').val(JSON.stringify(default_schema, null, "  "));
+	$('#genOptions').val(JSON.stringify(default_options, null, "  "));
 	
 	
 	//Build the form
@@ -70,11 +125,13 @@ jQuery(document).ready(function(){
 	$('#codeSpace').text(default_form);
 	
 	$('#generator').submit(UpdateForm);
-	$('#genOptions').on('change', UpdateForm);
-	$('#genSchema').on('change', UpdateForm);
+	$('#genOptions').on('input propertychange paste', UpdateForm);
+	$('#genSchema').on('input propertychange paste', UpdateForm);
 	
 	function UpdateForm(e) {
-		e.preventDefault();
+		if(e.type != "paste") 
+			e.preventDefault();
+			
 		console.log("Updating...");
 		$('#formSpace').html(" ");
 		$('#codeSpace').html(" ");
@@ -117,11 +174,20 @@ jQuery(document).ready(function(){
 			case "email":
 				retVal = AddEmailField(options, sItem);
 				break;	
+			case "dropdown":
+				retVal = AddDropdownField(options, sItem);
+				break;	
 			case "hidden":
 				retVal = AddHiddenField(options, sItem);
 				break;	
 			case "submit":
 				retVal = AddSubmitButton(options, sItem);
+				break;		
+			case "reset":
+				retVal = AddResetButton(options, sItem);
+				break;	
+			case "fieldset":
+				retVal = AddFieldset(options, sItem);
 				break;					
 			default:
 				console.log("Unknown type: ", sItem);
@@ -130,10 +196,10 @@ jQuery(document).ready(function(){
 		return retVal;
 	}
 	function AddTextField(options, sItem) {
-		sItem.label = sItem.label || "";
+		sItem.label = sItem.label || "Undefined";
 		sItem.class = sItem.class || "";
 		sItem.id = sItem.id || "";
-		sItem.name = sItem.name || "";
+		sItem.name = sItem.name || "txtField";
 		sItem.value = sItem.value || "";
 		sItem.required = sItem.required || "N";
 		sItem.placeholder = sItem.placeholder || "";
@@ -155,10 +221,10 @@ jQuery(document).ready(function(){
 	}
 
 	function AddEmailField(options, sItem) {
-		sItem.label = sItem.label || "";
+		sItem.label = sItem.label || "Undefined";
 		sItem.class = sItem.class || "";
 		sItem.id = sItem.id || "";
-		sItem.name = sItem.name || "";
+		sItem.name = sItem.name || "email";
 		sItem.value = sItem.value || "";
 		sItem.required = sItem.required || "N";
 		sItem.placeholder = sItem.placeholder || "";
@@ -180,11 +246,56 @@ jQuery(document).ready(function(){
 				' />\r\n\t\t\t</div>\r\n\t\t</div>\r\n\t</div>\r\n';
 	}
 	
-	function AddHiddenField(options, sItem) {
-		sItem.label = sItem.label || "";
+	function AddDropdownField(options, sItem) {
+		sItem.label = sItem.label || "Undefined";
 		sItem.class = sItem.class || "";
 		sItem.id = sItem.id || "";
-		sItem.name = sItem.name || "";
+		sItem.name = sItem.name || "txtField";
+		sItem.selected = sItem.selected || "";
+		
+		if (typeof sItem.options !== 'undefined' && sItem.options.length > 0) {
+			var asembleOptions = "";
+			sItem.options.forEach(function (entry) {
+				asembleOptions += AddSelectOptions(entry, sItem.selected);
+			});
+			
+			return 	'\t<div class="form-group">\r\n'+
+					'\t\t<label for="form-control '+sItem.name+'" '+
+						'class="'+((options.layout === "horizontal") ? "col-sm-2 " : "" )+'control-label">'+
+						sItem.label+
+					'</label>\r\n'+
+					'\t\t<div class="'+((options.layout === "horizontal") ? "col-sm-10 " : "" )+'">\r\n'+
+					'\t\t\t<select '+
+						'class="form-control '+sItem.class+'" '+
+						'id="'+sItem.id+'" '+
+						'name="'+sItem.name+'">\r\n'+
+						asembleOptions +
+					'\t\t\t</select>\r\n\t\t</div>\r\n\t</div>\r\n';
+		}	
+	}
+	
+	function AddSelectOptions(oItem, select) {
+		oItem.name = oItem.name || "opt";
+		oItem.value = oItem.value || "Undefined";
+		oItem.class = oItem.class || "";
+		oItem.id = oItem.id || "";
+		
+		var selected = (oItem.selected === "true" || oItem.name === select);
+		
+		return 	'\t\t\t\t<option '+
+					'class="form-control '+oItem.class+'" '+
+					'id="'+oItem.id+'" '+
+					'name="'+oItem.name+'" '+
+					((selected == true) ? 'selected="selected"': '') + '>'+
+					oItem.value+
+					'</option>\r\n';
+	}
+	
+	function AddHiddenField(options, sItem) {
+		sItem.label = sItem.label || "Undefined";
+		sItem.class = sItem.class || "";
+		sItem.id = sItem.id || "";
+		sItem.name = sItem.name || "txtHidden";
 		sItem.value = sItem.value || "";
 		sItem.required = sItem.required || "N";
 		return 	'\t<input type="'+sItem.type+'" '+
@@ -199,8 +310,8 @@ jQuery(document).ready(function(){
 	function AddSubmitButton(options, sItem) {
 		sItem.class = sItem.class || "";
 		sItem.id = sItem.id || "";
-		sItem.name = sItem.name || "";
-		sItem.value = sItem.value || "";
+		sItem.name = sItem.name || "submitBtn";
+		sItem.value = sItem.value || "Save";
 		return 	'\t<div class="'+((options.layout === "horizontal") ? "col-sm-offset-2 col-sm-10 " : "" )+'">\r\n'+
 				'\t\t<input type="'+sItem.type+'" '+
 					'class="btn btn-default '+sItem.class+'" '+
@@ -208,5 +319,43 @@ jQuery(document).ready(function(){
 					'value="'+sItem.value+'" '+
 					'name="'+sItem.name+'" '+
 				' />\r\n\t</div>\r\n';
+	}
+	function AddResetButton(options, sItem) {
+		sItem.class = sItem.class || "";
+		sItem.id = sItem.id || "";
+		sItem.name = sItem.name || "resetBtn";
+		sItem.value = sItem.value || "Reset";
+		return 	'\t<div class="'+((options.layout === "horizontal") ? "col-sm-offset-2 col-sm-10 " : "" )+'">\r\n'+
+				'\t\t<input type="'+sItem.type+'" '+
+					'class="btn btn-default '+sItem.class+'" '+
+					'id="'+sItem.id+'" '+
+					'value="'+sItem.value+'" '+
+					'name="'+sItem.name+'" '+
+				' />\r\n\t</div>\r\n';
+	}
+	
+	function AddFieldset(options, sItem) {
+		sItem.label = sItem.label || "Undefined";
+		sItem.name = sItem.name || "fieldset";
+		sItem.inherit = sItem.inherit || "false";
+		
+		if (typeof sItem.fields !== 'undefined' && sItem.fields.length > 0) {
+			var asembleFields = "";
+			sItem.fields.forEach(function (entry) {
+				entry.name = entry.name || "undefined";
+				
+				if(sItem.inherit == "true") {
+					entry.name = sItem.name + '.' + entry.name;
+				}
+				asembleFields += AddField(options, entry);
+			});
+			
+			return 	'\t<fieldset name="'+
+						sItem.name +
+						'">\r\n\t\t\<legend>'+
+						sItem.label+'</legend>\r\n'+
+						asembleFields +
+					'\t</fieldset>\r\n';
+		}	
 	}
 });
