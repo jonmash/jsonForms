05b7af2353ee0f42fb190039c51fc1943abf7a15
Jon Mash <jonmash@gmail.com>
2014-12-13 22:47:13 -0500

    Change to jQuery for building DOM

--- js/myForms.js
+++ js/myForms.js
@@ -1,8 +1,11 @@
+jQuery(document).ready(function() {
+
+
+
+});
+
 jQuery(document).ready(function(){
-	
 
-
-	
 	var default_options= {
 		title: "My Magic Form",
 		action: "#",
@@ -164,11 +167,12 @@ jQuery(document).ready(function(){
 	$('#genSchema').val(JSON.stringify(default_schema, null, "  "));
 	$('#genOptions').val(JSON.stringify(default_options, null, "  "));
 	
-	
 	//Build the form
 	var default_form = BuildForm(default_options, default_schema);
 	$('#formSpace').html(default_form);
-	$('#codeSpace').text(default_form);
+	$('#codeSpace').text(default_form.html());
+	makeReadable();
+	prettyPrint();
 	
 	$('#generator').submit(UpdateForm);
 	$('#genOptions').on('input propertychange paste', UpdateForm);
@@ -186,26 +190,40 @@ jQuery(document).ready(function(){
 			var sch = JSON.parse($('#genSchema').val());
 			var frm = BuildForm(opt, sch)
 			$('#formSpace').html(frm);
-			$('#codeSpace').text(frm);
+			$('#codeSpace').removeClass('prettyprinted').text(frm.html());
+			makeReadable();
+			prettyPrint();
 		} catch(e) {
 			$('#formSpace').html('<p class="bg-danger" style="padding:25px; margin-top:25px;">Failed to parse JSON. Check your Markup!</p>');
-			$('#codeSpace').html('<p class="bg-danger" style="padding:25px; margin-top:25px;">Failed to parse JSON. Check your Markup!</p>');
+			$('#codeSpace').html('<p class="bg-danger" style="">Failed to parse JSON. Check your Markup!</p>');
 			console.log("Failed!");
 		}
 	};
 		
 	function BuildForm(options, schema) {
 		//TODO: Check options object here
-		var text = "";
-		text += '<h1>'+options.title+'</h1>\r\n';
-		text += '<form action="'+options.action+'" class="'+((options.layout === "horizontal") ? "form-horizontal " : "" )+''+options.class+'" id="'+options.id+'" method="'+options.method+'" role="form">\r\n';
+		var container = $('<div />');
+		
+		var heading = 	$('<h1 />')
+						.text(options.title);
+
+		var form = 		$('<form />')
+						.attr("action", options.action)
+						.addClass(((options.layout === "horizontal") ? "form-horizontal " : "" ))
+						.addClass(options.class)
+						.addClass('rsvp_form')
+						.attr('id', options.id)
+						.attr('method', options.method)
+						.attr('role', 'form');
+					
 		schema.forEach(function (entry) {
-			text += AddField(options, entry);
+			form = form.append(AddField(options, entry));
 		});
 		
-		text += '</form>\r\n';
+		container = container.append(heading).append(form);
+		
 		if(options.validate === "Y") { $('.rsvp_form').validate(); }
-		return text;
+		return container;
 	}
 	
 	function AddField(options, sItem) {
@@ -247,6 +265,7 @@ jQuery(document).ready(function(){
 		}
 		return retVal;
 	}
+	
 	function AddTextField(options, sItem) {
 		sItem.label = sItem.label || "Undefined";
 		sItem.class = sItem.class || "";
@@ -256,20 +275,28 @@ jQuery(document).ready(function(){
 		sItem.required = sItem.required || "N";
 		sItem.placeholder = sItem.placeholder || "";
 		
-		return 	'\t<div class="form-group">\r\n'+
-				'\t\t<label for="form-control '+sItem.name+'" '+
-					'class="'+((options.layout === "horizontal") ? "col-sm-2 " : "" )+'control-label">'+
-					sItem.label+
-				'</label>\r\n'+
-				'\t\t<div class="'+((options.layout === "horizontal") ? "col-sm-10 " : "" )+'">\r\n'+
-				'\t\t\t<input type="'+sItem.type+'" '+
-					'class="form-control '+sItem.class+'" '+
-					'id="'+sItem.id+'" '+
-					'value="'+sItem.value+'" '+
-					'name="'+sItem.name+'" '+
-					'placeholder="'+sItem.placeholder+'"'+
-					((sItem.required === "Y") ? 'required' : '')+
-				' />\r\n\t\t</div>\r\n\t</div>\r\n';
+		var grp  = 	$('<div />').addClass('form-group');
+		var lbl  = 	$('<label />')
+					.attr('for', sItem.name)
+					.addClass(((options.layout === "horizontal") ? "col-sm-2 " : "" ))
+					.addClass('control-label')
+					.text(sItem.label);
+		var clmn = 	$('<div />').addClass(((options.layout === "horizontal") ? "col-sm-10 " : "" ));
+		var fld  = 	$('<input />')
+					.attr('type', 'text')
+					.addClass('form-control')
+					.addClass(sItem.class)
+					.attr('id', sItem.id)
+					.attr('value', sItem.value)
+					.attr('name', sItem.name)
+					.attr('placeholder', sItem.placeholder)
+					.prop('required',(sItem.required === "Y"));
+					
+		grp  = grp.append(lbl);
+		clmn = clmn.append(fld);
+		grp  = grp.append(clmn);
+		
+		return grp;
 	}
 
 	function AddEmailField(options, sItem) {
@@ -281,21 +308,31 @@ jQuery(document).ready(function(){
 		sItem.required = sItem.required || "N";
 		sItem.placeholder = sItem.placeholder || "";
 		
-		return 	'\t<div class="form-group">\r\n'+
-					'\t\t<label for="form-control '+sItem.name+'" '+
-						'class="'+((options.layout === "horizontal") ? "col-sm-2 " : "" )+'control-label">'+
-						sItem.label+'</label>\r\n'+
-					'\t\t<div class="'+((options.layout === "horizontal") ? "col-sm-10 " : "" )+'controls">\r\n'+
-						'\t\t\t<div class="input-group">\r\n'+
-							'\t\t\t\t<span class="input-group-addon">@</span>\r\n'+
-							'\t\t\t\t<input type="'+sItem.type+'" '+
-								'class="form-control '+sItem.class+'" '+
-								'id="'+sItem.id+'" '+
-								'value="'+sItem.value+'" '+
-								'name="'+sItem.name+'" '+
-								'placeholder="'+sItem.placeholder+'"'+
-								((sItem.required === "Y") ? 'required' : '')+
-				' />\r\n\t\t\t</div>\r\n\t\t</div>\r\n\t</div>\r\n';
+		var grp  = 	$('<div />').addClass('form-group');
+		var lbl  = 	$('<label />')
+					.attr('for', sItem.name)
+					.addClass(((options.layout === "horizontal") ? "col-sm-2 " : "" ))
+					.addClass('control-label')
+					.text(sItem.label);
+		var clmn = 	$('<div />').addClass(((options.layout === "horizontal") ? "col-sm-10 " : "" ));
+		var igrp = 	$('<div />').addClass('input-group');
+		var span =  $('<span />').addClass('input-group-addon').text('@');
+		var fld  = 	$('<input />')
+					.attr('type', 'text')
+					.addClass('form-control')
+					.addClass(sItem.class)
+					.attr('id', sItem.id)
+					.attr('value', sItem.value)
+					.attr('name', sItem.name)
+					.attr('placeholder', sItem.placeholder)
+					.prop('required',(sItem.required === "Y"));
+
+		grp  = grp.append(lbl);
+		igrp = igrp.append(span).append(fld);
+		clmn = clmn.append(igrp);
+		grp  = grp.append(clmn);
+					
+		return grp;
 	}
 	
 	function AddDropdownField(options, sItem) {
@@ -306,23 +343,29 @@ jQuery(document).ready(function(){
 		sItem.selected = sItem.selected || "";
 		
 		if (typeof sItem.options !== 'undefined' && sItem.options.length > 0) {
-			var asembleOptions = "";
+			
+			var grp  = 	$('<div />').addClass('form-group');
+			var lbl  = 	$('<label />')
+						.attr('for', sItem.name)
+						.addClass(((options.layout === "horizontal") ? "col-sm-2 " : "" ))
+						.addClass('control-label')
+						.text(sItem.label);
+			var clmn = 	$('<div />').addClass(((options.layout === "horizontal") ? "col-sm-10 " : "" ));
+			var sel  =  $('<select />')
+						.addClass('form-control')
+						.addClass(sItem.class)
+						.attr('id', sItem.id)
+						.attr('name', sItem.name);
+						
 			sItem.options.forEach(function (entry) {
-				asembleOptions += AddSelectOptions(entry, sItem.selected);
+				sel = sel.append(AddSelectOptions(entry, sItem.selected));
 			});
 			
-			return 	'\t<div class="form-group">\r\n'+
-					'\t\t<label for="form-control '+sItem.name+'" '+
-						'class="'+((options.layout === "horizontal") ? "col-sm-2 " : "" )+'control-label">'+
-						sItem.label+
-					'</label>\r\n'+
-					'\t\t<div class="'+((options.layout === "horizontal") ? "col-sm-10 " : "" )+'">\r\n'+
-					'\t\t\t<select '+
-						'class="form-control '+sItem.class+'" '+
-						'id="'+sItem.id+'" '+
-						'name="'+sItem.name+'">\r\n'+
-						asembleOptions +
-					'\t\t\t</select>\r\n\t\t</div>\r\n\t</div>\r\n';
+			grp  = grp.append(lbl);
+			clmn = clmn.append(sel);
+			grp  = grp.append(clmn);
+			
+			return grp;
 		}	
 	}
 	
@@ -334,13 +377,16 @@ jQuery(document).ready(function(){
 		
 		var selected = (oItem.selected === "true" || oItem.name === select);
 		
-		return 	'\t\t\t\t<option '+
-					'class="form-control '+oItem.class+'" '+
-					'id="'+oItem.id+'" '+
-					'name="'+oItem.name+'" '+
-					((selected == true) ? 'selected="selected"': '') + '>'+
-					oItem.value+
-					'</option>\r\n';
+		var opt = 	$('<option />')
+					.addClass('form-control')
+					.addClass(oItem.class)
+					.attr('id', oItem.id)
+					.attr('name', oItem.name)
+					.text(oItem.value);
+		if(selected)
+			opt = opt.prop('selected', true);
+		
+		return opt;
 	}
 	
 	function AddHiddenField(options, sItem) {
@@ -349,14 +395,16 @@ jQuery(document).ready(function(){
 		sItem.id = sItem.id || "";
 		sItem.name = sItem.name || "txtHidden";
 		sItem.value = sItem.value || "";
-		sItem.required = sItem.required || "N";
-		return 	'\t<input type="'+sItem.type+'" '+
-					'class="form-control '+sItem.class+'" '+
-					'id="'+sItem.id+'" '+
-					'value="'+sItem.value+'" '+
-					'name="'+sItem.name+'" '+
-					((sItem.required === "Y") ? 'required' : '')+
-				' />\r\n';
+		
+		var fld  = 	$('<input />')
+					.attr('type', 'hidden')
+					.addClass('form-control')
+					.addClass(sItem.class)
+					.attr('id', sItem.id)
+					.attr('value', sItem.value)
+					.attr('name', sItem.name);
+					
+		return fld;
 	}
 
 	function AddSubmitButton(options, sItem) {
@@ -364,28 +412,44 @@ jQuery(document).ready(function(){
 		sItem.id = sItem.id || "";
 		sItem.name = sItem.name || "submitBtn";
 		sItem.value = sItem.value || "Save";
-		return 	'\t<div class="form-group">\r\n'+
-				'\t\t<div class="'+((options.layout === "horizontal") ? "col-sm-offset-2 col-sm-10 " : "" )+'">\r\n'+
-				'\t\t\t<input type="'+sItem.type+'" '+
-					'class="btn btn-default '+sItem.class+'" '+
-					'id="'+sItem.id+'" '+
-					'value="'+sItem.value+'" '+
-					'name="'+sItem.name+'" '+
-				' />\r\n\t\t</div>\r\n\t</div>\r\n';
+		
+		var grp  = 	$('<div />').addClass('form-group');
+		var clmn = 	$('<div />').addClass(((options.layout === "horizontal") ? "col-sm-offset-2 col-sm-10 " : "" ));
+		var fld  = 	$('<input />')
+					.attr('type', 'submit')
+					.addClass('btn')
+					.addClass('btn-default')
+					.addClass(sItem.class)
+					.attr('id', sItem.id)
+					.attr('value', sItem.value)
+					.attr('name', sItem.name);
+					
+		clmn = clmn.append(fld);
+		grp  = grp.append(clmn);
+		
+		return grp;
 	}
 	function AddResetButton(options, sItem) {
 		sItem.class = sItem.class || "";
 		sItem.id = sItem.id || "";
 		sItem.name = sItem.name || "resetBtn";
 		sItem.value = sItem.value || "Reset";
-		return 	'\t<div class="form-group">\r\n'+
-				'\t\t<div class="'+((options.layout === "horizontal") ? "col-sm-offset-2 col-sm-10 " : "" )+'">\r\n'+
-				'\t\t\t<input type="'+sItem.type+'" '+
-					'class="btn btn-default '+sItem.class+'" '+
-					'id="'+sItem.id+'" '+
-					'value="'+sItem.value+'" '+
-					'name="'+sItem.name+'" '+
-				' />\r\n\t\t</div>\r\n\t</div>\r\n';
+		
+		var grp  = 	$('<div />').addClass('form-group');
+		var clmn = 	$('<div />').addClass(((options.layout === "horizontal") ? "col-sm-offset-2 col-sm-10 " : "" ));
+		var fld  = 	$('<input />')
+					.attr('type', 'reset')
+					.addClass('btn')
+					.addClass('btn-default')
+					.addClass(sItem.class)
+					.attr('id', sItem.id)
+					.attr('value', sItem.value)
+					.attr('name', sItem.name);
+					
+		clmn = clmn.append(fld);
+		grp  = grp.append(clmn);
+		
+		return grp;
 	}
 	
 	function AddCheckboxList(options, sItem) {
@@ -394,30 +458,42 @@ jQuery(document).ready(function(){
 		
 		if (typeof sItem.options !== 'undefined' && sItem.options.length > 0) {
 			var asembleFields = "";
+			
+			var grp  = 	$('<div />').addClass('form-group');
+			var lbl  = 	$('<label />')
+						.attr('for', sItem.name)
+						.addClass(((options.layout === "horizontal") ? "col-sm-2 " : "" ))
+						.addClass('control-label')
+						.text(sItem.label);
+			var clmn = 	$('<div />')
+						.addClass(((options.layout === "horizontal") ? "col-sm-10 " : "" ))
+						.addClass('controls');
+			var igrp =  $('<div />').addClass('input-group');
+			
 			sItem.options.forEach(function (entry) {
 				entry.name = sItem.name+'[]';
 				entry.value = entry.value || "undefined";
 				entry.selected = entry.selected || "false";
 				
 				var selected = (entry.selected == "true");
-				asembleFields += 	'\t\t\t\t<div class="checkbox">\r\n'+
-									'\t\t\t\t\t<label>\r\n'+
-									'\t\t\t\t\t\t<input type="checkbox" value="" name="'+
-									entry.name +'" '+
-									((selected == true) ? 'checked="checked"':'') + '>\r\n'+
-									'\t\t\t\t\t\t'+entry.value + '\r\n'+
-									'\t\t\t\t\t</label>\r\n\t\t\t\t</div>\r\n';
+				
+				var div = 	$('<div />').addClass('checkbox');
+				var lbl2 = 	$('<label />').text(entry.value);
+				var inp = 	$('<input />')
+							.attr('type', 'checkbox')
+							.attr('name', entry.name);
+				
+				if(selected) inp = inp.prop('checked', true);
+							
+				lbl2 = lbl2.prepend(inp);
+				div = div.append(lbl2);
+				igrp = igrp.append(div);
 			});
 			
-			return 	'\t<div class="form-group">\r\n'+
-					'\t\t<label for="form-control '+sItem.name+'" '+
-						'class="'+((options.layout === "horizontal") ? "col-sm-2 " : "" )+'control-label">'+
-						sItem.label+'</label>\r\n'+
-					'\t\t<div class="'+((options.layout === "horizontal") ? "col-sm-10 " : "" )+'controls">\r\n'+
-						'\t\t\t<div class="input-group">\r\n'+
-						asembleFields + 
-					'\t\t\t</div>\r\n\t\t</div>\r\n\t</div>\r\n';
-					
+			clmn = clmn.append(igrp);
+			grp = grp.append(lbl).append(clmn);
+			
+			return grp;					
 		}	
 	}
 	
@@ -427,30 +503,42 @@ jQuery(document).ready(function(){
 		
 		if (typeof sItem.options !== 'undefined' && sItem.options.length > 0) {
 			var asembleFields = "";
+			
+			var grp  = 	$('<div />').addClass('form-group');
+			var lbl  = 	$('<label />')
+						.attr('for', sItem.name)
+						.addClass(((options.layout === "horizontal") ? "col-sm-2 " : "" ))
+						.addClass('control-label')
+						.text(sItem.label);
+			var clmn = 	$('<div />')
+						.addClass(((options.layout === "horizontal") ? "col-sm-10 " : "" ))
+						.addClass('controls');
+			var igrp =  $('<div />').addClass('input-group');
+			
 			sItem.options.forEach(function (entry) {
 				entry.name = sItem.name+'[]';
 				entry.value = entry.value || "undefined";
 				entry.selected = entry.selected || "false";
 				
 				var selected = (entry.selected == "true");
-				asembleFields += 	'\t\t\t\t<div class="radio">\r\n'+
-									'\t\t\t\t\t<label>\r\n'+
-									'\t\t\t\t\t\t<input type="radio" value="" name="'+
-									entry.name +'" '+
-									((selected == true) ? 'checked="checked"':'') + '>\r\n'+
-									'\t\t\t\t\t\t'+entry.value + '\r\n'+
-									'\t\t\t\t\t</label>\r\n\t\t\t\t</div>\r\n';
+				
+				var div = 	$('<div />').addClass('radio');
+				var lbl2 = 	$('<label />').text(entry.value);
+				var inp = 	$('<input />')
+							.attr('type', 'radio')
+							.attr('name', entry.name);
+				
+				if(selected) inp = inp.prop('checked', true);
+							
+				lbl2 = lbl2.prepend(inp);
+				div = div.append(lbl2);
+				igrp = igrp.append(div);
 			});
 			
-			return 	'\t<div class="form-group">\r\n'+
-					'\t\t<label for="form-control '+sItem.name+'" '+
-						'class="'+((options.layout === "horizontal") ? "col-sm-2 " : "" )+'control-label">'+
-						sItem.label+'</label>\r\n'+
-					'\t\t<div class="'+((options.layout === "horizontal") ? "col-sm-10 " : "" )+'controls">\r\n'+
-						'\t\t\t<div class="input-group">\r\n'+
-						asembleFields + 
-					'\t\t\t</div>\r\n\t\t</div>\r\n\t</div>\r\n';
-					
+			clmn = clmn.append(igrp);
+			grp = grp.append(lbl).append(clmn);
+			
+			return grp;					
 		}	
 	}
 	
@@ -461,21 +549,50 @@ jQuery(document).ready(function(){
 		
 		if (typeof sItem.fields !== 'undefined' && sItem.fields.length > 0) {
 			var asembleFields = "";
+			
+			var  fs  = 	$('<fieldset />')
+						.attr('name', sItem.name);
+			var lgnd = 	$('<legend />').text(sItem.label);
+			
+			fs = fs.append(lgnd);
+			
 			sItem.fields.forEach(function (entry) {
 				entry.name = entry.name || "undefined";
 				
 				if(sItem.inherit == "true") {
 					entry.name = sItem.name + '.' + entry.name;
 				}
-				asembleFields += AddField(options, entry);
+				fs = fs.append(AddField(options, entry));
 			});
 			
-			return 	'\t<fieldset name="'+
-						sItem.name +
-						'">\r\n\t\t\<legend>'+
-						sItem.label+'</legend>\r\n'+
-						asembleFields +
-					'\t</fieldset>\r\n';
+			return fs;
 		}	
 	}
 });
+
+
+function makeReadable() {
+	var readableHTML = $('#codeSpace').text();
+	var lb = '\r\n';
+	var htags = ["<html","</html>","</head>","<title","</title>","<meta","<link","<style","</style>","</body>"];
+	for (i=0; i<htags.length; ++i) {
+		var hhh = htags[i];
+		readableHTML = readableHTML.replace(new RegExp(hhh,'gi'),lb+hhh);
+	}
+	var btags = ["</div>","</span>","</form>","</fieldset>","<br>","<br />","<hr","<pre","</pre>","<blockquote","</blockquote>","<ul","</ul>","<ol","</ol>","<li","<dl","</dl>","<dt","</dt>","<dd","</dd>","<\!--","<table","</table>","<caption","</caption>","<th","</th>","<tr","</tr>","<td","</td>","<script","</script>","<noscript","</noscript>"];
+	for (i=0; i<btags.length; ++i) {
+		var bbb = btags[i];
+		readableHTML = readableHTML.replace(new RegExp(bbb,'gi'),lb+bbb);
+	}
+	var ftags = ["<label","</label>","<legend","</legend>","<object","</object>","<embed","</embed>","<select","</select>","<option","<option","<input","<textarea","</textarea>"];
+	for (i=0; i<ftags.length; ++i) {
+		var fff = ftags[i];
+		readableHTML = readableHTML.replace(new RegExp(fff,'gi'),lb+fff);
+	}
+	var xtags = ["<body","<head","<div","<span","<p","<form","<fieldset"];
+	for (i=0; i<xtags.length; ++i) {
+		var xxx = xtags[i];
+		readableHTML = readableHTML.replace(new RegExp(xxx,'gi'),lb+xxx);
+	}
+	$('#codeSpace').text(readableHTML);
+}
